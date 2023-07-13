3492d4cffbdc04e4a0740b4b5f07dbe8567a1518
Daniel Anestopoulos <dstops97@gmail.com>
2023-07-12 20:04:45 -0400

    PageLoad functional

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,1 @@
-alert("Setup Success");
+(()=>{"use strict";!function(){const e=document.getElementById("content"),t=document.createElement("h1");t.textContent="Le Pizz";const n=document.createElement("p");n.textContent="This is the best Restaurant";const c=document.createElement("img");c.src="/assets/pizza.jpg",e.appendChild(t),e.appendChild(n),e.appendChild(c)}()})();
--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
-alert('Setup Success');
+import pageLoad from "./pageLoad";
+
+pageLoad();
